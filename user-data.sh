55b28db5087cd6cf1d341d5218e2a60a38fb9172
chidam333 <chidam3chain@gmail.com>
2026-05-10 18:17:11 +0530

    Update Nginx configuration and user-data script for module installation

--- user-data.sh
+++ user-data.sh
@@ -44,7 +44,7 @@ dnf install -y nodejs npm
 # Install Nginx.
 dnf -y install nginx
 
-dnf -y install libnginx-mod-http-js
+dnf -y install nginx-module-njs
 
 # Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key="$TAILSCALE_AUTH_KEY"
